b9a90eef18c3f27d9ee7a86b3af2df45857b04eb
rizqiindah03 <rizqiindah32@gmail.com>
2022-12-09 15:50:17 +0700

    Add project files.

---
+++ exercise4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise4
+{
+    class Stack
+    {
+        private string[] indah;
+        int element;
+        int max;
+        int top;
+        public Stack(int size)
+        {
+            top = -1;
+            max = size;
+            indah = new string[size];
+        }
+        public bool isEmpty()
+        {
+            if (top == null)
+                return true;
+            return false;
+        }
+        public void Push(int element)
+        {
+            if (top == max -1)
+            {
+                Console.WriteLine("Stack is full");
+            }
+            else
+            {
+                indah[top++] = 
+            }
+        }
+    }
+}
